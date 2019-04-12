465368497280b4bccfcd88c3333885d0374f5c2f
Hasan Guner <hasanguner07@gmail.com>
2019-04-13 02:16:02 +0300

    Made more readable.

--- src/main/kotlin/net/hasanguner/search/AbstractSearch.kt
+++ src/main/kotlin/net/hasanguner/search/AbstractSearch.kt
@@ -1,5 +1,6 @@
 package net.hasanguner.search
 
+import net.hasanguner.search.datastructure.Edge
 import net.hasanguner.search.datastructure.Node
 import java.util.*
 
@@ -13,53 +14,44 @@ abstract class AbstractSearch : Search {
         search(fringe.apply { add(root) }, mutableSetOf(), goal)
             .let { SearchResult(goal.getShortestPath(), it) }
 
-    private tailrec fun search(
-        fringe: Queue<Node>,
-        explored: MutableSet<Node>,
-        goal: Node
-    ): Set<Node> =
-        if (fringe.isEmpty()) explored
-        else {
-            val node = fringe.remove()
-            if (node == goal) {
-                goal.apply {
-                    parent = node.parent
-                    cost = node.cost
-                }
-                explored.apply { add(node) }
-            } else {
-                search(
-                    fringe.apply { addAdjacencies(node, explored) },
-                    explored.apply { add(node) },
-                    goal
-                )
+    private tailrec fun search(fringe: Queue<Node>, explored: MutableSet<Node>, goal: Node): Set<Node> {
+        if (fringe.isEmpty()) return explored
+        val node = fringe.remove()
+        if (node == goal) {
+            goal.apply {
+                parent = node.parent
+                cost = node.cost
             }
+            return explored.apply { add(node) }
         }
+        fringe.apply { addAdjacencies(node, explored) }
+        explored.apply { add(node) }
+        return search(fringe, explored, goal)
+    }
 
     private fun Queue<Node>.addAdjacencies(parentNode: Node, explored: MutableSet<Node>): Boolean {
-        val nodeCosts = parentNode.adjacencies.map {
-            it.target to costCalculator.calculate(parentNode, it.target, it.cost)
-        }.toMap()
+        val nodeCosts: Map<Node, Int> = parentNode.adjacencies
+            .map { it.target to costCalculator.calculate(parentNode, it.target, it.cost) }
+            .toMap()
 
         parentNode.adjacencies
             .filter { it.target in this }
             .filter { nodeCosts[it.target]!! < it.target.cost }
-            .map {
-                it.target.apply {
-                    parent = parentNode
-                    cost = nodeCosts[this]!!
-                }
-            }.also { removeAll(it) }
+            .mapToUpdatedTargets(parentNode, nodeCosts)
+            .also { removeAll(it) }
             .also { addAll(it) }
 
         return parentNode.adjacencies
             .filterNot { adjacency -> adjacency.target in explored }
-            .map { adjacency ->
-                adjacency.target.apply {
-                    parent = parentNode
-                    cost = nodeCosts[this]!!
-                }
-            }.let { addAll(it) }
+            .mapToUpdatedTargets(parentNode, nodeCosts)
+            .let { addAll(it) }
+    }
+
+    private fun Iterable<Edge>.mapToUpdatedTargets(parentNode: Node, costMap: Map<Node, Int>): List<Node> = map {
+        it.target.apply {
+            parent = parentNode
+            cost = costMap[this]!!
+        }
     }
 
 }
